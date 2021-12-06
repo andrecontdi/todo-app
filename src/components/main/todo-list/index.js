fb1f78be9fa8ffa601d188f11515a41e84da1b88
Andrea L. Contreras D <a.contreras@globant.com>
2021-12-06 17:19:17 -0300

    feat: completing and deleting todos

--- src/components/main/todo-list/index.js
+++ src/components/main/todo-list/index.js
@@ -2,30 +2,54 @@ import React from "react";
 import { TodoItem } from "./todo-item";
 import './todo-list.css'
 
-function TodoList() {
-    const todos = [
-        {id: 1, text: 'Daily meeting with team', completed: false},
-        {id: 2, text: 'Pay for rent', completed: true},
-        {id: 3, text: 'Check emails', completed: false},
-        {id: 4, text: 'Lunch with Emma', completed: true},
-        {id: 5, text: 'Meditation', completed: false},
-        {id: 6, text: 'Beat Thanos', completed: false},
-        {id: 7, text: 'Call mom', completed: false},
-    ]
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [
+                {id: 1, text: 'Daily meeting with team', completed: false},
+                {id: 2, text: 'Pay for rent', completed: true},
+                {id: 3, text: 'Check emails', completed: false},
+                {id: 4, text: 'Lunch with Emma', completed: true},
+                {id: 5, text: 'Meditation', completed: false},
+                {id: 6, text: 'Beat Thanos', completed: false},
+                {id: 7, text: 'Call mom', completed: false},
+            ]
+        }
+    }
 
-    return (
-        <section>
-            <h3 className="section-header">Today's tasks</h3>
-            <ul className="todo-list"> { 
-                todos.map((todo) => (
-                    <TodoItem 
-                        key={todo.id} 
-                        todo={todo}
-                    />
-                ))} 
-            </ul>
-        </section>
-    )
+    handleCompleteTodo(todoId) {
+        const todoIndex = this.state.todos.findIndex((todo) => todo.id === todoId);
+        const todosCopy = [...this.state.todos]
+        todosCopy[todoIndex].completed = !todosCopy[todoIndex].completed;
+        this.setState({todos: todosCopy})
+
+    }
+
+    handleDeleteTodo(todoId) {
+        const todoIndex = this.state.todos.findIndex((todo) => todo.id === todoId);
+        const todosCopy = [...this.state.todos]
+        todosCopy.splice(todoIndex,1)
+        this.setState({ todos: todosCopy })
+    }
+
+    render() {
+        return (
+            <section>
+                <h3 className="section-header">Today's tasks</h3>
+                <ul className="todo-list"> { 
+                    this.state.todos.map((todo) => (
+                        <TodoItem 
+                            key={todo.id} 
+                            todo={todo}
+                            handleCompleteTodo={() => this.handleCompleteTodo(todo.id)}
+                            handleDeleteTodo={() => this.handleDeleteTodo(todo.id)}
+                        />
+                    ))} 
+                </ul>
+            </section>
+        )
+    }
 }
 
 export { TodoList }
--- src/components/main/todo-list/todo-item/index.js
+++ src/components/main/todo-list/todo-item/index.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { FiTrash } from "react-icons/fi";
 import './todo-item.css';
 
-function TodoItem({ todo }) {
-    const handleChecked = () => {
-        console.log(' Completed todo:', todo.id)
-    }
-
+function TodoItem({ todo, handleCompleteTodo, handleDeleteTodo }) {
     return (
         <li className="todo-list__item">
-            <div className="checkbox--round">
-                <input type="checkbox" id={`checkbox-${todo.id}`} checked={todo.completed} onChange={handleChecked}/>
-                <label htmlFor={`checkbox-${todo.id}`}></label>
+            <div className="todo-list__container">
+                <div className="checkbox--round">
+                    <input type="checkbox" id={`checkbox-${todo.id}`} checked={todo.completed} onChange={handleCompleteTodo}/>
+                    <label htmlFor={`checkbox-${todo.id}`}></label>
+                </div>
+                <div className="todo-text--container">
+                    <p className="todo-text">{todo.text}</p>
+                </div>
             </div>
-            <p className="todo-list__item--text">{todo.text}</p>
+            <button className="delete-button"><FiTrash style={{color: '#86a2eb', fontSize: '20px'}} onClick={handleDeleteTodo}/></button>
         </li>
     )
 }
