5ea8c59ee1d4feeac6b991a0292a87be3c53e9f1
Andrea L. Contreras D <a.contreras@globant.com>
2021-12-06 21:44:07 -0300

    feat: handle todo counting feat: handling todo completed counting

--- src/components/main/todo-list/index.js
+++ src/components/main/todo-list/index.js
@@ -1,55 +1,26 @@
 import React from "react";
-import { TodoItem } from "./todo-item";
-import './todo-list.css'
+import "./todo-list.css";
 
-class TodoList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todos: [
-                {id: 1, text: 'Daily meeting with team', completed: false},
-                {id: 2, text: 'Pay for rent', completed: true},
-                {id: 3, text: 'Check emails', completed: false},
-                {id: 4, text: 'Lunch with Emma', completed: true},
-                {id: 5, text: 'Meditation', completed: false},
-                {id: 6, text: 'Beat Thanos', completed: false},
-                {id: 7, text: 'Call mom', completed: false},
-            ]
-        }
-    }
-
-    handleCompleteTodo(todoId) {
-        const todoIndex = this.state.todos.findIndex((todo) => todo.id === todoId);
-        const todosCopy = [...this.state.todos]
-        todosCopy[todoIndex].completed = !todosCopy[todoIndex].completed;
-        this.setState({todos: todosCopy})
-
-    }
-
-    handleDeleteTodo(todoId) {
-        const todoIndex = this.state.todos.findIndex((todo) => todo.id === todoId);
-        const todosCopy = [...this.state.todos]
-        todosCopy.splice(todoIndex,1)
-        this.setState({ todos: todosCopy })
-    }
-
-    render() {
-        return (
-            <section>
-                <h3 className="section-header">Today's tasks</h3>
-                <ul className="todo-list"> { 
-                    this.state.todos.map((todo) => (
-                        <TodoItem 
-                            key={todo.id} 
-                            todo={todo}
-                            handleCompleteTodo={() => this.handleCompleteTodo(todo.id)}
-                            handleDeleteTodo={() => this.handleDeleteTodo(todo.id)}
-                        />
-                    ))} 
-                </ul>
-            </section>
-        )
-    }
+function TodoList({ children }) {
+  return (
+    <section>
+      <h3 className="section-header">Today's tasks</h3>
+      <ul className="todo-list">
+        { children }
+      </ul>
+    </section>
+  );
 }
 
-export { TodoList }
+export { TodoList };
+
+/* {
+  todos.map((todo) => (
+    <TodoItem
+      key={todo.id}
+      todo={todo}
+      handleCompleteTodo={() => handleCompleteTodo(todo.id)}
+      handleDeleteTodo={() => handleDeleteTodo(todo.id)}
+    />
+  ));
+} */
